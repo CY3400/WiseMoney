c76d828a507cbc58d7eb61960af4752fb9d10a87
Charbel Yammine <charbelymn@gmail.com>
2025-10-07 18:39:12 +0300

    First commit

---
+++ WiseMoney/src/main/java/com/Wise_Money/WiseMoney/WiseMoneyApplication.java
@@ -0,0 +1,13 @@
+package com.Wise_Money.WiseMoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiseMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiseMoneyApplication.class, args);
+	}
+
+}
---
+++ WiseMoney/src/test/java/com/Wise_Money/WiseMoney/WiseMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Wise_Money.WiseMoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiseMoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
